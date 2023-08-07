826a567efa897704c6ec58efd633e80081e63d99
w1y0n0 <wiyono30@gmail.com>
2023-08-07 08:57:17 +0700

    add comment & file config.json

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
 
+/*
+BluePrint Rumah
+Tukang
+Alat Tukang (Paku, Palu, dll)
+
+Architecture                v
+Programmer                  v
+Utillity (State Management, DIO, dll)
+
+
+Tips: Bagaimana membuat Aplikasi yang tetap menerapkan Architecture,
+Dan hanya menggunakan State Management bawaan Flutter
+
+Provider
+Bloc
+Getx
+Riverpod
+
+Skala Kecil / Skala Besar
+=> Hanya utk mempermudah state management
+
+MVC Architecture
+  UI      View              Form            LoginView
+  Logic   Controller        OnButtonClick   LoginController
+  State                                     LoginState
+  Service                                   LoginService        HTTP Request | LocalStorage
+MVVM Architecture
+BLOC Architecture
+*/
 void main() {
   runApp(const MyApp());
 }
